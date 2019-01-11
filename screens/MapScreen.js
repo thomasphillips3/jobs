fa52488b3385962118af6a708daf09f9c0d9ecbc
Thomas Phillips <thomasphillips3@gmail.com>
2019-01-11 15:26:04 -0800

    Fetch nearby jobs from Indeed API
    The API key given in the course isn't working and I can't get one for myself yet. Because of this, as of this commit, the call doesn't work.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { View, ActivityIndicator } from "react-native";
 import { MapView } from "expo";
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import { fetchJobs } from "../actions/job_actions";
+import * as actions from '../actions'
 
 class MapScreen extends Component {
   state = {
@@ -21,6 +26,10 @@ class MapScreen extends Component {
     this.setState({ region });
   }
 
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
+  }
+
   render() {
     if (!this.state.mapLoaded) {
       return (
@@ -36,10 +45,26 @@ class MapScreen extends Component {
           style={{ flex: 1 }} 
           region={this.state.region} 
           onRegionChangeComplete={this.onRegionChangeComplete}
+        />
+        <View style={styles.buttonContainer}>
+          <Button 
+            large
+            title="Search around here"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }} 
+            onPress={this.onButtonPress}
           />
+        </View>
       </View>
     );
   }
 }
-
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+export default connect(null, actions)(MapScreen);
