56812d9f53188ef8a55ac633e1a3e1f391eb792a
Thomas Phillips <thomasphillips3@gmail.com>
2019-01-11 14:05:38 -0800

    Display a map around Santa Cruz

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, ActivityIndicator } from "react-native";
+import { MapView } from "expo";
 
 class MapScreen extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  };
+
+  componentDidMount() {
+    this.setState({ mapLoaded: true });
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region });
+  }
+
   render() {
-    return(
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView 
+          style={{ flex: 1 }} 
+          region={this.state.region} 
+          onRegionChangeComplete={this.onRegionChangeComplete}
+          />
       </View>
     );
   }
