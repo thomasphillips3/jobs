464932744935e355174b63b44ce933b61d062048
Thomas Phillips <thomasphillips3@gmail.com>
2019-01-11 17:07:43 -0800

    Navigate to deck screen after tapping search

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -22,18 +22,16 @@ const buildJobsUrl = (zip) => {
   return `${JOB_ROOT_URL}${query}`;
 }
 
-export const fetchJobs = (region) => async (dispatch) => {
+export const fetchJobs = (region, callback) => async (dispatch) => {
   try {
     let zip = await reverseGeocode(region);
     const url = buildJobsUrl(zip);
-    let {
-      data
-    } = await axios.get(url);
+    let { data } = await axios.get(url);
     dispatch({
       type: FETCH_JOBS,
       payload: data
     });
-
+    callback();
   } catch (e) {
     console.error(e);
   }
